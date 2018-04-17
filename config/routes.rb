0553f4ca17fd1df1677b297984b4e101d3dae12e
ronald vilorio <soccer23415@gmail.com>
2018-04-17 12:08:29 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :products,root to: only: [:index]
+  resources :products, only: [:index]
+  roo
   
 end
